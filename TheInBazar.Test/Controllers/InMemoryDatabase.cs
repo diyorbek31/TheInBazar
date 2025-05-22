44931969293c0d8679c4d1b60535cde5982ae2b1
00022067 <00022067wiut@gmail.com>
2025-05-22 12:59:03 +0500

    For CRUD operations integration tests implemented ,and got success

--- TheInBazar.Test/Controllers/InMemoryDatabase.cs
+++ TheInBazar.Test/Controllers/InMemoryDatabase.cs
@@ -100,11 +100,76 @@ namespace TheInBazar.Test.Controllers
             var response = await client.PostAsJsonAsync("api/users", newUser);
             response.EnsureSuccessStatusCode();
 
+            // firts checking is it avaiable this kind of user or not
             var db = _factory.Services.CreateScope().ServiceProvider.GetRequiredService<AppDbContext>();
             var savedUser = db.Users.FirstOrDefault(u => u.Email == "asad1106");
 
             savedUser.Should().NotBeNull();
             savedUser!.Firstname.Should().Be("Asadbek");
+        }
+        [Fact]
+        public async Task OnPutUser_WhenValidUpdate_ModifyNewUser()
+        {
+            using (var scope = _factory.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                db.Users.Add(new User 
+                { 
+                    Id = 1,
+                    Firstname = "Diyorbek",
+                    Lastname = "Juraev", 
+                    Email = "newemail@me.com",
+                    Password = "passwordold" 
+                });
+                db.SaveChanges();
+            }
+            var newUser = new User()
+            {
+                Id = 1,
+                Firstname = "Omina",
+                Lastname = "Bahodirova",
+                Email = "new1221",
+                Password = "1234"
+            };
+
+            var client = _factory.CreateClient();
+
+            var response = await client.PutAsJsonAsync("api/users",newUser);
+            response.EnsureSuccessStatusCode();
+
+            var scope2 = _factory.Services.CreateScope();
+            var db2 = scope2.ServiceProvider.GetRequiredService<AppDbContext>();
+            var user = db2.Users.First(u => u.Id == 1);
+            user.Firstname.Should().Be("Omina");
+        }
+        [Fact]
+        public async Task OnDeleteUser_WhenValidId_ShouldDeleteUser()
+        {
+            using (var scope = _factory.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                db.Users.Add(new User
+                {
+                    Id = 15,
+                    Firstname = "Nozimjon",
+                    Lastname = "Usmonaliyev",
+                    Email = "deleted@me.com",
+                    Password = "blablabla"
+                });
+                db.SaveChanges();
+            }
+            var client = _factory.CreateClient();
+
+            var response = await client.DeleteAsync("api/users/15");
+            response.EnsureSuccessStatusCode();
+
+            using(var scope2 = _factory.Services.CreateScope())
+            {
+                var db2 = scope2.ServiceProvider.GetRequiredService<AppDbContext>();
+                var user = db2.Users.FirstOrDefault(u => u.Id == 15);
+                user.Should().BeNull(); // it should be deleted
+            }
+            
         }
     }
 }
